3213ffddf19800ea16f81ed7ebd6aa77b9ca82d0
Solomon Peter <peterkingsley26@gmail.com>
2012-03-05 15:17:46 +0530

    kk

--- git.py
+++ git.py
@@ -1,7 +1,7 @@
 import os
 
 def git(op):
-    s = os.walk('/home/solomon/pythontask')
+    s = os.walk('/home/solomon/pythonex')
     os.system("ls -a")
     os.system("git add .")
     
@@ -13,7 +13,7 @@ def git(op):
     print "\t\tRemote ->"
     os.system("git remote")
 #    os.system("git remote add origin git@github.com:SolomonPeter26/pythontask.git")
-#    os.system("git push origin master")
+    os.system("git push origin master")
 
 
 if __name__ == "__main__" :
