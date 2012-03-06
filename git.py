471ae1217a2cede6aa0fb9b4f02a6c0806afbf89
Solomon Peter <peterkingsley26@gmail.com>
2012-03-07 02:26:18 +0530

    git

--- git.py
+++ git.py
@@ -1,18 +1,51 @@
 import os
 
+     
 def git(op):
-    s = os.walk('/home/solomon/pythonex')
-    os.system("ls -a")
-    os.system("git add .")
+    #os.getcwd()
+    #print s
+#    print os.listdir(os.getcwd())
+
+            #change directory
+    s = os.chdir((raw_input("Enter Folder:")))
+#    os.getcwd()
+    print "\n\n"
+    print os.listdir(os.getcwd())
+
+
+
     
-    stri="git commit -m "+ (raw_input("Enter message for commit:"))
-    os.system(stri)
+#    c = str("cd " + str(s))
+#    os.system(c)
+#    os.system("ls -a")
+
+            # only INITIALISE GIT IN FOLDER
+    os.system("git init")
+
+    
+            #add files for commit
+    os.system("git add .")
+    stri="git commit -m \" " + (raw_input("Enter message for commit:")) + "\""
+    
+            #view log of commits
     print "\t\tLocal ->"
     os.system("git log")
+    os.system(stri)   
     
-    print "\t\tRemote ->"
+            #view status of repository
+    print "\t\tStatus ->"
+    os.system("git status")
+    
+            #view files which have been commited
+    print "\t\tCommited Files ->"
+#    os.system("git ls-files")
+    
+    
+#    print "\t\tRemote ->"
     os.system("git remote")
-    os.system("git remote add origin git@github.com:SolomonPeter26/python-examples.git")
+    
+        
+ #   os.system("git remote add origin git@github.com:SolomonPeter26/python-examples.git")
     os.system("git push origin master")
 
 
